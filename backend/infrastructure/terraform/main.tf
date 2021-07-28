ff7e0c472de3751d51e8601fe93c1862cd5bb8cf
Jerry Chen <misodope@cap-rx.com>
2021-07-27 23:42:45 -0400

    Updating tf

--- backend/infrastructure/terraform/main.tf
+++ backend/infrastructure/terraform/main.tf
@@ -12,6 +12,8 @@ terraform {
 provider "aws" {
   profile = "default"
   region = "${var.region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
 
 resource "aws_instance" "app_server" {
--- backend/infrastructure/terraform/variables.tf
+++ backend/infrastructure/terraform/variables.tf
@@ -3,3 +3,6 @@ variable "region" {
     type = string
     default = "us-east-1"
 }
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
